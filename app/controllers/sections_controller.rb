04960754e47208bd47369a0c64fa6810613b53cc
James <james.randolph2016@gmail.com>
2018-07-29 17:42:46 -0400

    sections controller filled out

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,24 +4,46 @@ class SectionsController < ApplicationController
   end
 
   def show
+    @section = Section.find(params[:id])
   end
 
   def new
+    @section = Section.new
   end
 
   def create
+    @section = Section.new(section_params)
+      if @section.save
+        flash[:notice] = "Section created successfully"
+        redirect_to(sections_path)
+      else
+        render('new')
+      end
   end
 
   def edit
+    @section = Section.find(params[:id])
   end
 
   def update
+    @section = Section.find(params[:id])
   end
 
   def delete
+    @section = Section.find(params[:id])
   end
 
   def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    flash[:notice] = "Section destroyed successfully"
+    redirect_to(sections_path)
   end
-  
+
+  private
+
+  def section_params
+    params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
+  end
+
 end
