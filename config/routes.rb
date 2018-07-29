0e80c12789280c96b25358c47a794020fcf5b75a
James <james.randolph2016@gmail.com>
2018-07-29 13:23:33 -0400

    playing with routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'demo/hello'
   get 'demo/other_hello'
 
+  get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
