aae726f234dbe45ea95f3a827782f222e3c9d878
James <james.randolph2016@gmail.com>
2018-07-29 17:34:19 -0400

    building out controller in pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,24 +5,37 @@ class PagesController < ApplicationController
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(page_params)
+      if @page.save
+          flash[:notice] = "Page successfully created!"
+          redirect_to(pages_path)
+      else
+        render('new')
+      end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
 
   def destroy
+    @page = Page.find(params[:id])
   end
 
 end
