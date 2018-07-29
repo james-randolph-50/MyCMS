d5b5a1e66163c9c4008ab10fdea57f96a44dced1
James <james.randolph2016@gmail.com>
2018-07-29 14:10:24 -0400

    attributes for subhects

--- db/migrate/20180729180758_create_subjects.rb
+++ db/migrate/20180729180758_create_subjects.rb
@@ -1,6 +1,9 @@
 class CreateSubjects < ActiveRecord::Migration[5.1]
   def change
     create_table :subjects do |t|
+      t.string "name"
+      t.integer "position"
+      t.boolen "visible", :default => false
 
       t.timestamps
     end
