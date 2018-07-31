61904800927ead1f09c7a95b5474f47fbb5d50f8
James <james.randolph2016@gmail.com>
2018-07-31 12:11:14 -0400

    yung validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,6 +8,10 @@ class Page < ApplicationRecord
     scope :sorted, lambda { order("position ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
     
-
+    validates_presence_of :name
+    validates_length_of :name, maximum => 255
+    validates_presence_of :permanlink
+    validates_length_of :permanlink, :within => 3..255
+    validates_uniqueness_of :permanlink
 
 end
