10dd5977cd5851def67cf2aa6f05466573bfed56
James <james.randolph2016@gmail.com>
2018-07-31 20:33:02 -0400

    login logic

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,4 +1,7 @@
 class AccessController < ApplicationController
+
+  layout 'admin'
+
   def menu
   end
 
@@ -6,9 +9,23 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(:username => params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+  end
+
+  if authorized_user
+    session[:user_id] = authorized_user.id
+    flash[:notice] = "You are now logged in."
+    redirect_to(admin_path)
+  else
+    flash.now[:notice] = "invalid username/password combo"
+    render('login')
   end
 
   def logout
   end
-  
+
 end
