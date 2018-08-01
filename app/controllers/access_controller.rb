6d1100fd153d6c9bd4df8104d535fd646b3acb28
James <james.randolph2016@gmail.com>
2018-07-31 20:41:25 -0400

    before actions for life

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,8 @@ class AccessController < ApplicationController
 
   layout 'admin'
 
+  before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
+
   def menu
   end
 
@@ -26,6 +28,10 @@ class AccessController < ApplicationController
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to(access_login_path)
   end
+
 
 end
